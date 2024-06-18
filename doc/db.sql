ffea0f7e0d55770dd3aa1d4b470c94ceda01b6bf
wangbao <197235339@qq.com>
2024-06-18 11:36:23 +0800

    增加sql

--- doc/db.sql
+++ doc/db.sql
@@ -3821,5 +3821,4 @@ create table if not exists t_tenant_info
 
 
 
-INSERT INTO t_login_account (id, user_name, login_phone, pwd, is_delete, create_time, create_by, update_time, update_by, trace_id, tenant_id, version_num, user_pwd) VALUES (1, '王五', 18616771546, '21232F297A57A5A743894A0E4A801FC3', 0, '2024-05-31 16:20:50', null, '2024-06-03 16:36:52', null, null, 1001, 0, '21232F297A57A5A743894A0E4A801FC3');
 INSERT INTO t_login_account (id, user_name, login_phone, pwd, is_delete, create_time, create_by, update_time, update_by, trace_id, tenant_id, version_num, user_pwd) VALUES (2, 'admin', 18600000000, null, 0, '2024-05-31 19:23:34', null, '2024-06-03 16:41:05', null, null, 1001, 0, '21232F297A57A5A743894A0E4A801FC3');
