19f5ada22707e19ff3a538cf8a9f43bbe79853c7
wangbao <197235339@qq.com>
2024-07-05 13:58:16 +0800

    fix 修改订单状态日期返回错误

--- src/main/java/com/olivia/peanut/aps/api/entity/apsOrderGoodsStatusDate/ApsOrderGoodsStatusDateDto.java
+++ src/main/java/com/olivia/peanut/aps/api/entity/apsOrderGoodsStatusDate/ApsOrderGoodsStatusDateDto.java
@@ -13,7 +13,7 @@ import lombok.experimental.Accessors;
  * 订单商品状态表(ApsOrderGoodsStatusDate)查询对象返回
  *
  * @author peanut
- * @since 2024-06-14 10:26:59
+ * @since 2024-07-05 13:44:45
  */
 //@Accessors(chain=true)
 @Getter
@@ -22,12 +22,12 @@ import lombok.experimental.Accessors;
 @SuppressWarnings("serial")
 public class ApsOrderGoodsStatusDateDto extends BaseEntityDto {
 
-  @NotNull(message = "商品不能为空", groups = {InsertCheck.class, UpdateCheck.class})
+  @NotNull(message = "${column.comment}不能为空", groups = {InsertCheck.class, UpdateCheck.class})
   private Long orderId;
   /***
    *  商品ID
    */
-  @NotNull(message = "商品不能为空", groups = {InsertCheck.class, UpdateCheck.class})
+  @NotNull(message = "商品ID不能为空", groups = {InsertCheck.class, UpdateCheck.class})
   private Long goodsId;
   private String goodsName;
   /***
@@ -39,7 +39,7 @@ public class ApsOrderGoodsStatusDateDto extends BaseEntityDto {
   /***
    *  期望制造时间
    */
-//  @NotNull(message = "期望制造时间不能为空", groups = {InsertCheck.class, UpdateCheck.class})
+  @NotNull(message = "期望制造时间不能为空", groups = {InsertCheck.class, UpdateCheck.class})
   private LocalDate expectMakeTime;
   /***
    *  实际制造时间
@@ -49,11 +49,34 @@ public class ApsOrderGoodsStatusDateDto extends BaseEntityDto {
   /***
    *  工厂ID
    */
-  @NotNull(message = "工厂不能为空", groups = {InsertCheck.class, UpdateCheck.class})
+  @NotNull(message = "工厂ID不能为空", groups = {InsertCheck.class, UpdateCheck.class})
   private Long factoryId;
-
-
+  /***
+   *  状态索引
+   */
+  @NotNull(message = "状态索引不能为空", groups = {InsertCheck.class, UpdateCheck.class})
   private Integer statusIndex;
+  /***
+   *  预计开始时间
+   */
+  @NotNull(message = "预计开始时间不能为空", groups = {InsertCheck.class, UpdateCheck.class})
+  private LocalDate expectMakeBeginTime;
+  /***
+   *  预计结束时间
+   */
+  @NotNull(message = "预计结束时间不能为空", groups = {InsertCheck.class, UpdateCheck.class})
+  private LocalDate expectMakeEndTime;
+  /***
+   *  实际开始时间
+   */
+  @NotNull(message = "实际开始时间不能为空", groups = {InsertCheck.class, UpdateCheck.class})
+  private LocalDate actualMakeBeginTime;
+  /***
+   *  实际结束时间
+   */
+  @NotNull(message = "实际结束时间不能为空", groups = {InsertCheck.class, UpdateCheck.class})
+  private LocalDate actualMakeEndTime;
+
 }
 
 
--- src/main/java/com/olivia/peanut/aps/model/ApsOrderGoodsStatusDate.java
+++ src/main/java/com/olivia/peanut/aps/model/ApsOrderGoodsStatusDate.java
@@ -1,52 +1,66 @@
 package com.olivia.peanut.aps.model;
 
 
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.olivia.sdk.utils.BaseEntity;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import com.olivia.sdk.utils.BaseEntity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
-
+import com.baomidou.mybatisplus.annotation.TableName;
 /**
  * 订单商品状态表(ApsOrderGoodsStatusDate)表实体类
  *
  * @author peanut
- * @since 2024-06-14 10:26:59
+ * @since 2024-07-05 13:44:45
  */
-@Accessors(chain = true)
+@Accessors(chain=true)
 @Getter
 @Setter
 //@SuppressWarnings("serial")
 @TableName("aps_order_goods_status_date")
-public class ApsOrderGoodsStatusDate extends BaseEntity {
-
-  private Long orderId;
-  /***
-   *  商品ID
-   */
-  private Long goodsId;
-  /***
-   *  订单状态
-   */
-  private Long goodsStatusId;
-  /***
-   *  期望制造时间
-   */
-  private LocalDate expectMakeBeginTime;
-
-  private LocalDate expectMakeEndTime;
-  /***
-   *  实际制造时间
-   */
-  private LocalDate actualMakeBeginTime;
-  private LocalDate actualMakeEndTime;
-  /***
-   *  工厂ID
-   */
-  private Long factoryId;
-
-  private Integer statusIndex;
+public class ApsOrderGoodsStatusDate  extends  BaseEntity {
+     private Long orderId;
+        /***
+            *  商品ID
+            */
+     private Long goodsId;
+        /***
+            *  订单状态
+            */
+     private Long goodsStatusId;
+        /***
+            *  期望制造时间
+            */
+     private LocalDate expectMakeTime;
+        /***
+            *  实际制造时间
+            */
+     private LocalDate actualMakeTime;
+        /***
+            *  工厂ID
+            */
+     private Long factoryId;
+        /***
+            *  状态索引
+            */
+     private Integer statusIndex;
+        /***
+            *  预计开始时间
+            */
+     private LocalDate expectMakeBeginTime;
+        /***
+            *  预计结束时间
+            */
+     private LocalDate expectMakeEndTime;
+        /***
+            *  实际开始时间
+            */
+     private LocalDate actualMakeBeginTime;
+        /***
+            *  实际结束时间
+            */
+     private LocalDate actualMakeEndTime;
 
 }
 
