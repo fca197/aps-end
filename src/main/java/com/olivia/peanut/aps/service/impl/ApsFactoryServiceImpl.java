0ab06ee61ffcc6e5b1e0f90979ad5442e9a079a0
wangbao <197235339@qq.com>
2024-07-11 16:50:44 +0800

    perf(ApsFactoryService): 增加Cache缓存, 暂定10分钟,最多20条

--- src/main/java/com/olivia/peanut/aps/service/impl/ApsFactoryServiceImpl.java
+++ src/main/java/com/olivia/peanut/aps/service/impl/ApsFactoryServiceImpl.java
@@ -4,6 +4,8 @@ import static java.lang.Boolean.TRUE;
 
 import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.olivia.peanut.aps.api.entity.apsProcessPath.ApsProcessPathDto;
 import com.olivia.peanut.aps.api.entity.apsProcessPath.ApsProcessPathQueryListReq;
 import com.olivia.peanut.aps.service.ApsFactoryService;
@@ -22,7 +24,11 @@ import com.olivia.sdk.utils.$;
 import com.olivia.sdk.utils.RunUtils;
 import com.olivia.sdk.utils.model.WeekInfo;
 import jakarta.annotation.Resource;
-import java.util.*;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
@@ -46,40 +52,62 @@ public class ApsFactoryServiceImpl implements ApsFactoryService {
   @Resource
   ApsProcessPathService apsProcessPathService;
 
+  Cache<String, List<WeekInfo>> factoryWeekInfoCache = CacheBuilder.newBuilder().maximumSize(20).expireAfterWrite(Duration.ofMinutes(10)).build();
+  Cache<String, List<ShiftItem>> factoryShiftCache = CacheBuilder.newBuilder().maximumSize(20).expireAfterWrite(Duration.ofMinutes(10)).build();
+  Cache<String, Map<Long, ApsProcessPathDto>> factoryPathCache = CacheBuilder.newBuilder().maximumSize(20).expireAfterWrite(Duration.ofMinutes(10)).build();
+
   @Override
   public FactoryConfigRes getFactoryConfig(FactoryConfigReq req) {
     if (log.isDebugEnabled()) {
       log.debug("getFactoryConfig req :{}", JSON.toJSONString(req));
     }
     List<Runnable> runnableList = new ArrayList<>();
-    FactoryConfigRes res = new FactoryConfigRes().setFactoryId(req.getFactoryId()).setFactoryName(req.getFactoryName());
+    Long factoryId = req.getFactoryId();
+    FactoryConfigRes res = new FactoryConfigRes().setFactoryId(factoryId).setFactoryName(req.getFactoryName());
     if (TRUE.equals(req.getGetWeek())) {
       runnableList.add(() -> {
-        $.assertTrueCanIgnoreException(Objects.isNull(req.getWeekBeginDate()) || Objects.isNull(req.getWeekEndDate()), "工厂查询日期不能为空");
-        List<WeekInfo> weekList = calendarService.getWeekList(req.getFactoryId(), req.getWeekBeginDate().toString(), req.getWeekEndDate().toString());
-        weekList.removeIf(w -> Boolean.FALSE.equals(w.getIsWorkDay()));
-        weekList.sort(Comparator.comparing(WeekInfo::getCurrentDate));
-        res.setWeekList(weekList);
+        try {
+          List<WeekInfo> retList = factoryWeekInfoCache.get(factoryId.toString() + req.getWeekBeginDate() + req.getWeekEndDate(), () -> {
+            List<WeekInfo> weekList = calendarService.getWeekList(factoryId, req.getWeekBeginDate(), req.getWeekEndDate());
+            weekList.removeIf(w -> Boolean.FALSE.equals(w.getIsWorkDay()));
+            weekList.sort(Comparator.comparing(WeekInfo::getCurrentDate));
+            return weekList;
+          });
+          res.setWeekList(retList);
+        } catch (Exception e) {
+          log.error("factoryWeekInfoCache {} error:{}", factoryId, e.getMessage(), e);
+        }
       });
     }
     if (TRUE.equals(req.getGetShift())) {
       runnableList.add(() -> {
-        Shift shift = shiftService.getOne(new LambdaQueryWrapper<Shift>().eq(Shift::getFactoryId, req.getFactoryId()), false);
-        $.requireNonNullCanIgnoreException(shift, req.getFactoryName() + "排产版本工厂没有班次");
-        List<ShiftItem> shiftItemList = shiftItemService.list(new LambdaQueryWrapper<ShiftItem>().eq(ShiftItem::getShiftId, shift.getId()).orderByAsc(ShiftItem::getBeginTime));
-        Long dayWorkSecond = ProcessUtils.getDayWorkSecond($.copyList(shiftItemList, ShiftItemVo.class));
-        res.setShiftItemList(shiftItemList).setDayWorkSecond(dayWorkSecond);
+        try {
+          List<ShiftItem> shiftItemList = factoryShiftCache.get("dayWorkSecond-" + factoryId, () -> {
+            Shift shift = shiftService.getOne(new LambdaQueryWrapper<Shift>().eq(Shift::getFactoryId, factoryId), false);
+            $.requireNonNullCanIgnoreException(shift, req.getFactoryName() + "排产版本工厂没有班次");
+            return shiftItemService.list(new LambdaQueryWrapper<ShiftItem>().eq(ShiftItem::getShiftId, shift.getId()).orderByAsc(ShiftItem::getBeginTime));
+          });
+          Long dayWorkSecond = ProcessUtils.getDayWorkSecond($.copyList(shiftItemList, ShiftItemVo.class));
+          res.setShiftItemList(shiftItemList).setDayWorkSecond(dayWorkSecond);
+        } catch (Exception e) {
+          log.error("shiftItemList error {} error:{}", factoryId, e.getMessage(), e);
+        }
       });
     }
     if (TRUE.equals(req.getGetPath())) {
       runnableList.add(() -> {
-        Map<Long, ApsProcessPathDto> pathDtoMap = apsProcessPathService.queryList(
-                new ApsProcessPathQueryListReq().setData(new ApsProcessPathDto().setFactoryId(req.getFactoryId()).setIsDefault(req.getGetPathDefault())))
-            .getDataList().stream().collect(Collectors.toMap(BaseEntityDto::getId, Function.identity()));
-        res.setPathDtoMap(pathDtoMap);
+        try {
+          Map<Long, ApsProcessPathDto> pathDtoMap = factoryPathCache.get(req.getFactoryName(),
+              () -> apsProcessPathService.queryList(new ApsProcessPathQueryListReq().setData(new ApsProcessPathDto().setFactoryId(factoryId).setIsDefault(req.getGetPathDefault())))
+                  .getDataList().stream().collect(Collectors.toMap(BaseEntityDto::getId, Function.identity())));
+          res.setPathDtoMap(pathDtoMap);
+        } catch (Exception e) {
+          log.error("factoryPathCache {} error: {}", factoryId, e.getMessage(), e);
+        }
+
       });
     }
-    RunUtils.run("getFactoryConfig " + req.getFactoryId(), runnableList);
+    RunUtils.run("getFactoryConfig " + factoryId, runnableList);
     return res;
   }
 }
