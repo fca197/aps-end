00353bb4908d8ac1d2f863728dd9bafbe6c595cf
wangbao <197235339@qq.com>
2024-07-30 17:56:12 +0800

    build(打包): 拆分打包-运行jar和依赖zip

--- mvn.sh
+++ mvn.sh
@@ -2,6 +2,9 @@ export JAVA_HOME=/Users/wangbao/Library/Java/JavaVirtualMachines/corretto-17.0.8
 echo $JAVA_HOME
 mvn -DskipTests clean package -U -s ~/.m2/settings-read.xml
 
+zip -rj target/lib.zip target/lib/
+
+
 #mvn  -v
 
 #/Users/wangbao/Library/Java/JavaVirtualMachines/corretto-17.0.8.1/Contents/Home/bin/java   -jar ./target/app.jar
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 export JAVA_HOME=/Users/wangbao/Library/Java/JavaVirtualMachines/corretto-17.0.8.1/Contents/Home
 echo $JAVA_HOME
-mvn -DskipTests clean package
-/Users/wangbao/Library/Java/JavaVirtualMachines/corretto-17.0.8.1/Contents/Home/bin/java -jar  ./target/app-encrypted.jar
+#mvn -DskipTests clean package
+/Users/wangbao/Library/Java/JavaVirtualMachines/corretto-17.0.8.1/Contents/Home/bin/java -jar  ./target/app.jar
 #mvn  -v
 
 #/Users/wangbao/Library/Java/JavaVirtualMachines/corretto-17.0.8.1/Contents/Home/bin/java   -jar ./target/app.jar
