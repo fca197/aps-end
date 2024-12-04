dc7e22ebe55d354120353a8b7f5ef348014e4dda
wangbao <197235339@qq.com>
2024-12-04 18:00:36 +0800

    feat(jdk） 切换21

--- mvn.sh
+++ mvn.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=~/Library/Java/JavaVirtualMachines/corretto-17.0.8.1/Contents/Home
+export JAVA_HOME=~/Library/Java/JavaVirtualMachines/corretto-21.0.5/Contents/Home
 echo $JAVA_HOME
 mvn -DskipTests clean package -U -s ~/.m2/settings-read.xml
 
