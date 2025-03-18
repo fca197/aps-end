d4747f461e921bbd0a9c246852b22509dc7efcd3
wangbao <197235339@qq.com>
2025-03-18 14:37:40 +0800

    doc 增加说明文档

--- sql/delete-order-不分表.sql
+++ sql/delete-order-不分表.sql
@@ -1,2 +1,54 @@
-set global innodb_strict_mode = 0;
-flush privileges;
+truncate table aps_order;
+truncate table aps_order_goods;
+
+
+truncate table aps_order_goods_forecast_make;
+
+# rename table  aps_rolling_forecast_order_item to aps_rolling_forecast_order_item;
+# rename table  aps_order_goods_project_config to aps_order_goods_project_config;
+# rename table  aps_order_goods_status_date to aps_order_goods_status_date;
+# rename table  aps_order_goods_sale_config to aps_order_goods_sale_config;
+
+truncate table aps_order_goods_project_config;
+truncate table aps_order_goods_sale_config;
+truncate table aps_order_goods_status_date;
+truncate table aps_order_goods_bom;
+truncate table aps_rolling_forecast_order_item;
+
+
+truncate table aps_order_user;
+
+
+truncate table aps_scheduling_day_config_version;
+
+truncate table aps_scheduling_day_config_version_detail;
+
+truncate table aps_scheduling_day_config_version_detail_machine;
+truncate table aps_scheduling_day_config_version_detail_machine_use_time;
+
+
+truncate table aps_scheduling_goods_bom;
+
+truncate table aps_scheduling_goods_bom_total;
+
+truncate table aps_scheduling_goods_status_date;
+
+truncate table aps_scheduling_issue_item;
+
+truncate table aps_scheduling_version;
+
+truncate table aps_scheduling_version_capacity;
+
+truncate table aps_scheduling_version_day;
+
+truncate table aps_scheduling_version_item;
+
+truncate table aps_scheduling_version_limit;
+
+truncate table aps_goods_bom_buy_plan;
+
+truncate table aps_goods_bom_buy_plan_item;
+
+truncate table aps_rolling_forecast_order;
+
+
